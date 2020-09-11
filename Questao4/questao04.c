ef3684515d7bcdcea6b8b34a18bcefa8de55275a
Allysson Martins <martins.allysson@hotmail.com>
2020-09-11 04:13:33 -0300

    questao 4

--- Questao4/questao04.c
+++ Questao4/questao04.c
@@ -6,43 +6,39 @@ Fazer um programa para ler 5 valores e, em seguida, mostrar todos os valores
 lidos juntamente com o maior, o menor e a media dos valores. 
 */
 
-int main()
+int main(void)
 {
-    int qtdDeVetores;
-    int vetValores[qtdDeVetores];
+    int vetValores[5];
     int maiorValor;
     int menorValor;
     int somaValores;
     int mediaGeral;
 
-    printf("Digite a Quantidade de Valores: \n");
-    scanf("%d", &qtdDeVetores);
-
-    for (int i = 1; i <= qtdDeVetores; i++)
+    for (int i = 1; i <= 5; i++)
     {
         printf("Digite o %d numero: \n", i);
         scanf("%d", &vetValores[i]);
     }
 
-    maiorValor = vetValores[0];
-    for (int i = 1; i <= qtdDeVetores; i++)
+    maiorValor = vetValores[1];
+    for (int i = 0; i <= 5; i++)
     {
         if (vetValores[i] > maiorValor)
         {
             maiorValor = vetValores[i];
         }
     }
-    menorValor = vetValores[0];
-    for (int i = 1; i <= qtdDeVetores; i++)
+    menorValor = vetValores[1];
+    for (int i = 1; i <= 5; i++)
     {
         if (vetValores[i] < menorValor)
         {
             menorValor = vetValores[i];
         }
     }
-    for (int i = 1; i <= qtdDeVetores; i++)
+    for (int i = 1; i <= 5; i++)
     {
-        printf("%d valor é: %d \n", i, vetValores[1]);
+        printf("%d valor é: %d \n", i, vetValores[i]);
     }
     printf("O menor valor digitado foi: %d \n", menorValor);
     printf("O maior valor digitado foi: %d \n", maiorValor);
