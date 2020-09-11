89d054fbf45abaf0080a5b777bc0504322ebd132
Allysson Martins <martins.allysson@hotmail.com>
2020-09-10 21:31:50 -0300

    questao2

--- Questao2/questao02.c
+++ Questao2/questao02.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+02- Faça um programa para ler a nota da prova de 15 alunos e armazene num
+vetor, calcule e imprima a media geral.
+*/
+
 int main(void)
 {
-    int num1 = 3;
-    printf("O numero é: %d", num1);
+    int qtdAlunos = 15;
+    float notaAlunos[15];
+    int totalnotas;
+    float mediaGeral;
+
+    for (int i = 1; i < qtdAlunos; i++)
+    {
+        printf("Digite a nota do Aluno %d : \n", i);
+        scanf("%f", &notaAlunos[i]);
+        totalnotas += notaAlunos[i];
+    }
+    for (int i = 1; i < qtdAlunos; i++)
+    {
+        printf("A nota do Aluno %d é: %f \n", i, notaAlunos[i]);
+    }
 }
