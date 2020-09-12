7e97358a0c6d346bdd75ef155baecb60a9994774
Allysson Martins <martins.allysson@hotmail.com>
2020-09-12 02:37:35 -0300

    q13

---
+++ Questao13/questao13.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Escrever um programa que cadastre a altura, o peso, o cpf e sexo de algumas pessoas. 
+Com os dados cadastrados, em seguida localizar uma pessoas através do seu CPF 
+e imprimir o seu IMC.
+*/
