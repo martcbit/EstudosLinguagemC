f75c5b41eeed7917bc784817beedf3002b022ab7
Allysson Martins <martins.allysson@hotmail.com>
2020-09-21 22:06:32 -0300

    questao23final

---
+++ Questao23/questao23.c
@@ -0,0 +1,8 @@
+/*
+int i=99,j; ---> A variavel do tipo int i é declarada e inicializada com o valor 99.
+                A variavel j é declarada (tipo int)
+int *p; ---> O ponteiro p é criado.
+p = &i; ---> O ponteiro p armazena o endereço da variavel i.
+j = *p + 100; ---> A variavel j armazena o conteudo de i + 100, ao final 
+armazena o valor 199.
+*/
